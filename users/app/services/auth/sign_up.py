92f6fc8c0ce1aaf8cec5f05fff47561cd2f655bf
lokeshchauhan521 <lokesh.chauhan.nmg@gmail.com>
2024-12-13 19:59:52 +0530

    User servies : Responser updated

--- users/app/services/auth/sign_up.py
+++ users/app/services/auth/sign_up.py
@@ -4,16 +4,20 @@ from app.core.config.db import get_db
 from app.core.auth.security import hash_password
 from app.models.user import User
 from app.schemas.user import user as UserSchema
+from app.core.responser.responser import Responser
 
 
 class SignUp:
     def post(self, request: UserSchema.UserSignUp, db: Session = Depends(get_db)):
         db_user = db.query(User).filter(User.email == request.email).first()
+        data = { }
         if db_user:
-            raise HTTPException(
-                status_code=status.HTTP_400_BAD_REQUEST,
-                detail="Email already registered",
-            )
+            # return HTTPException(
+            #     status_code=status.HTTP_400_BAD_REQUEST,
+            #     detail="Email already registered",
+            # )
+            payload = Responser(data)
+            return payload.response_400()
 
         hashed_password = hash_password(request.password)
 
@@ -26,8 +30,11 @@ class SignUp:
         db.add(new_user)
         db.commit()
         db.refresh(new_user)
-
-        return {
-            "message": "User created successfully",
-            "user": {"email": new_user.email, "name": new_user.name},
-        }
+        data["email"] = new_user.email
+        data["name"] = new_user.name
+        # return {
+        #     "message": "User created successfully",
+        #     "user": {"email": new_user.email, "name": new_user.name},
+        # }
+        payload = Responser(data)
+        return payload.response_201()
--- users/app/services/get_users.py
+++ users/app/services/get_users.py
@@ -2,8 +2,16 @@ from fastapi import Depends
 from sqlalchemy.orm import Session
 from app.core.config.db import get_db
 from ..models import user as UserModel
+from app.core.responser.responser import Responser
 
 
 class GetUsers:
     def get(self, db: Session = Depends(get_db)):
-        return db.query(UserModel.User).all()
+        data = {}
+        data['data']=db.query(UserModel.User).all()
+        if not data:
+            payload =  Responser(data)
+            return payload.response_400()
+        else:
+            payload = Responser(data)
+            return payload.response_200()
