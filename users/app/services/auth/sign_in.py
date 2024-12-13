91025b590053b4babe14ae315ae7cc528ad91976
lokesh <lokesh.chauhan.nmg@gmail.com>
2024-12-13 09:34:46 -0800

    User Service : Responser added

--- users/app/services/auth/sign_in.py
+++ users/app/services/auth/sign_in.py
@@ -5,17 +5,23 @@ from app.core.config.db import get_db
 from app.core.auth.security import verify_password
 from app.core.auth.security import create_access_token
 from app.schemas.user import user as UserSchema
+from app.core.responser.responser import Responser
 
 
 class SignIn:
-    def post(self, request: UserSchema.UserLogin, db: Session = Depends(get_db)):
+    async def post(self, request: UserSchema.UserLogin, db: Session = Depends(get_db)):
+            user = db.query(User).filter(User.email == request.email).first()
+            data = {}
+            if user is None:
+                payload = Responser(data)
+                return payload.response_400()
 
-        user = db.query(User).filter(User.email == request.email).first()
-        if user is None:
-            raise HTTPException(status_code=400, detail="Invalid credentials")
+            if not verify_password(request.password, user.password):
+                payload = Responser(data)
+                return payload.response_400()
 
-        if not verify_password(request.password, user.password):
-            raise HTTPException(status_code=400, detail="Invalid credentials")
-
-        access_token = create_access_token(data={"sub": user.email})
-        return {"access_token": access_token, "token_type": "bearer"}
+            access_token = create_access_token(data={"sub": user.email})
+            if access_token:
+                data["token"] = access_token
+            payload = Responser(data)
+            return payload.response_200()
--- users/app/services/auth/sign_up.py
+++ users/app/services/auth/sign_up.py
@@ -8,33 +8,25 @@ from app.core.responser.responser import Responser
 
 
 class SignUp:
-    def post(self, request: UserSchema.UserSignUp, db: Session = Depends(get_db)):
-        db_user = db.query(User).filter(User.email == request.email).first()
-        data = { }
-        if db_user:
-            # return HTTPException(
-            #     status_code=status.HTTP_400_BAD_REQUEST,
-            #     detail="Email already registered",
-            # )
-            payload = Responser(data)
-            return payload.response_400()
+    async def post(self, request: UserSchema.UserSignUp, db: Session = Depends(get_db)):
+                db_user = db.query(User).filter(User.email == request.email).first()
+                data = {}
+                if db_user:
+                    payload = Responser(data)
+                    return payload.response_400()
 
-        hashed_password = hash_password(request.password)
+                hashed_password = hash_password(request.password)
 
-        new_user = User(
-            email=request.email,
-            name=request.name,
-            password=hashed_password,
-        )
+                new_user = User(
+                    email=request.email,
+                    name=request.name,
+                    password=hashed_password,
+                )
 
-        db.add(new_user)
-        db.commit()
-        db.refresh(new_user)
-        data["email"] = new_user.email
-        data["name"] = new_user.name
-        # return {
-        #     "message": "User created successfully",
-        #     "user": {"email": new_user.email, "name": new_user.name},
-        # }
-        payload = Responser(data)
-        return payload.response_201()
+                db.add(new_user)
+                db.commit()
+                db.refresh(new_user)
+                data["email"] = new_user.email
+                data["name"] = new_user.name
+                payload = Responser(data)
+                return payload.response_201()
