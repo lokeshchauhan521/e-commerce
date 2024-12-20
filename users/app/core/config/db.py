dd7aee260d2a8d2ec784ce0e6ff4c77c4702c190
deepak252 <deepakkudc16102000@gmail.com>
2024-12-20 13:08:36 +0530

    Compose, db update

--- users/app/core/config/db.py
+++ users/app/core/config/db.py
@@ -1,10 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from .environment import POSTGRES_DB_URL
+from .environment import USER_DB_URL
 
 
-engine = create_engine(POSTGRES_DB_URL)
+engine = create_engine(USER_DB_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
